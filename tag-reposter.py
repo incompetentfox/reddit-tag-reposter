87dd6ca029d1089cf9af4185bd2b90f6b6b3fb4b
incompetentfox <andrew@T430.Home>
2017-06-09 17:52:00 +0100

    Added feature to notify a user when new content is posted

--- tag-reposter.py
+++ tag-reposter.py
@@ -14,21 +14,43 @@ pword =                                         # password
 subreddit_title = "roadcam"                     # the subreddit to scan for posts
 repost_subreddit = "ukdashcam"                  # the subreddit to repost them to
 tagterm = '[uk]'                                # the tag term to search for
+notifyuser = [True,"username_to_pm"]            # if True, PM username on new posts
+r= praw.Reddit(client_id=script_id,client_secret=script_secret,password=pword,username=uname,user_agent="UK Dashcam repost script")
 
 import praw,time
 
 def main():
-    r= praw.Reddit(client_id=script_id,client_secret=script_secret,password=pword,username=uname,user_agent="UK Dashcam repost script")
 
+    newpostlist = []
     for submission in r.subreddit(subreddit_title).new(limit=1000):
         if tagterm in submission.title.lower():
             try:
                 print("Attempting to repost "+submission.title)
-                r.subreddit(repost_subreddit).submit(submission.title,url=submission.url,send_replies=False,resubmit=False)
-
+                r.subreddit(repost_subreddit).submit(submission.title, url=submission.url, send_replies=False,
+                                                     resubmit=False)
+                print("Success")
+                newpostlist.append(submission)
             except:
-                print("Failed to repost"+submission.title)
+                print("Failed to repost" + submission.title)
                 continue
-        time.sleep(5)
 
-main()
+            time.sleep(5)
+            notify(newpostlist)
+
+def notify(newpostlist):
+    submissions = ""
+        if notifyuser[0] == True:
+            for submission in newpostlist:
+                submissions += "[" + submission.title + "](" + submission.url + ")\n"
+
+            if submissions != "":
+                notifystring = "The following links have been added to /r/" + repost_subreddit + ":\n" + submissions
+                r.redditor(notifyuser[1]).message('New posts in /r/ukdashcam', notifystring)
+                print("Notification sent to " + notifyuser[1])
+            else:
+                print("No notifications to send")
+        else:
+            print("Notify flag set to false")
+
+        main()
+
